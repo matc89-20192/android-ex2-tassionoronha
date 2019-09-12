7e6e6df1e4116ccba7bd610a0a36d32a56479a0f
Tassio <tassionoronha@gmail.com>
2019-09-12 16:48:06 -0300

    add files with return limitation

--- app/src/main/java/matc89/exercicio2/MainActivity.java
+++ app/src/main/java/matc89/exercicio2/MainActivity.java
@@ -1,13 +1,69 @@
 package matc89.exercicio2;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private String nome = "";
+    private TextView textView;
+    private Button btnTrocar;
+    private static final int TELA_CONFIRMA = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        btnTrocar = (Button) findViewById(R.id.btnTrocar);
+        btnTrocar.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (TextUtils.isEmpty(nome)) {
+            textView.setText("Oi!");
+        } else {
+            textView.setText(String.format("Oi, %s!", nome));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("textoAtual", nome);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        nome = savedInstanceState.getString("textoAtual");
+        textView.setText(nome);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == TELA_CONFIRMA && resultCode == this.RESULT_OK) {
+            nome = data.getExtras().getString("novoNome");
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnTrocar) {
+            Intent intent = new Intent(MainActivity.this, OutraActivity.class);
+            intent.putExtra("novoNome", nome);
+            startActivityForResult(intent, TELA_CONFIRMA);
+        }
     }
 }
---
+++ app/src/main/java/matc89/exercicio2/OutraActivity.java
@@ -0,0 +1,90 @@
+package matc89.exercicio2;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class OutraActivity extends AppCompatActivity implements View.OnClickListener {
+    public EditText editText;
+    private String novoNome;
+    private Button btnConfirmar;
+    private Button btnCancelar;
+
+    protected  void listenButtons(){
+        Intent intent = getIntent();
+
+        novoNome = intent.getStringExtra("novoNome");
+        editText = (EditText) findViewById(R.id.editText);
+        editText.setText(novoNome, TextView.BufferType.EDITABLE);
+
+        editText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    editText.setText("");
+                }
+                return false;
+            }
+        });
+
+        btnConfirmar = (Button) findViewById(R.id.btnConfirmar);
+        btnCancelar = (Button) findViewById(R.id.btnCancelar);
+        btnCancelar.setOnClickListener(this);
+        btnConfirmar.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outra);
+        listenButtons();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String meutexto = savedInstanceState.getString("textoPendente");
+        editText.setText(meutexto);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("textoPendente", editText.getText().toString());
+    }
+
+    private void updateName(){
+        Intent data;
+        String nome = editText.getText().toString();
+        data = new Intent();
+        data.putExtra("novoNome", nome);
+        setResult(RESULT_OK, data);
+        finish();
+    }
+
+    private void cancelOperation(){
+        Intent data;
+        data = new Intent();
+        setResult(RESULT_CANCELED, data);
+        finish();
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.btnConfirmar:
+                updateName();
+                break;
+            case R.id.btnCancelar:
+                cancelOperation();
+                break;
+        }
+    }
+}
